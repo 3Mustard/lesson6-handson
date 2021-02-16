ac64336b2697f50f48de8aeffabfae41ba800bc6
Matthew Cullen <m23.cullen@gmail.com>
2021-02-16 16:36:04 -0500

    add Car.cs Model with property acc.

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+namespace lesson6_handson.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int NumberOfPassengers { get; set; }
+
+    }
+}
